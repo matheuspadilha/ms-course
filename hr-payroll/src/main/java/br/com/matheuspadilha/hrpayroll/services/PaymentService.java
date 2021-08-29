038f7b2a8187c89629745f7b04e5c208271fc67f
Matheus Padilha <mvpadilha94@gmail.com>
2021-08-29 20:26:56 -0300

    Configuring Eureka client

--- hr-payroll/src/main/java/br/com/matheuspadilha/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/br/com/matheuspadilha/hrpayroll/services/PaymentService.java
@@ -6,6 +6,8 @@ import br.com.matheuspadilha.hrpayroll.feignclients.WorkerFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Objects;
+
 @Service
 public class PaymentService {
     
@@ -15,6 +17,8 @@ public class PaymentService {
     public Payment getPayment(Long workerId, int days) {
         
         Worker worker = workerFeignClient.findById(workerId).getBody();
+        
+        Objects.requireNonNull(worker);
         
         return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
